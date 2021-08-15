fb3447cf790dc423607c97a3bcb8aaac11df8f73
Lucas <lucas.seifert@mmda.com.br>
2021-08-15 15:20:07 -0300

    Change calendar structure

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useCalendarContext } from "../../context/context";
+import { getMonthIdFromDate } from "../../utils";
 import Month from "../month/Month";
 
+
 function Calendar() {
+  const currentMonth = (new Date());
+  // Set the first day of the month as the month ID
+  const monthId = getMonthIdFromDate(currentMonth);
+
   return <section className='container'>
     <h1>Calendar</h1>
-    <Month />
+    <Month monthId={ monthId } />
   </section>
 }
 
