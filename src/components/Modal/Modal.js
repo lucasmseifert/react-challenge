34265d5d6a4d04af8e1c6e3b9b24366cd6e3723b
Lucas <lucas.seifert@mmda.com.br>
2021-08-16 11:10:50 -0300

    Add modal structure and basic styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
+import ReactDom from "react-dom";
+import { FaTimes } from "react-icons/fa";
+import './Modal.css';
 
-function Modal({children}) {
+function Modal({children, isOpen, setIsOpen}) {
 
-  return <div className='modal'>
-    {children}
-  </div>
+  const modalRef = useRef(null);
+
+  function clickHandler(event) {
+    // If this is true, the overlay was clicked, so we close the modal
+    if (event.target === modalRef.current) {
+      setIsOpen(false);
+    }
+  }
+
+  // Use a portal to prevent z-index issues
+  return ReactDom.createPortal(
+    <div className='modal' onClick={clickHandler} ref={modalRef}>
+      <div className='modal__content'>
+        <div className='modal__close'>
+          <button onClick={() => setIsOpen(false)}><FaTimes/></button>
+        </div>
+        {children}
+      </div>
+    </div>,
+    document.getElementById('modal-root')
+  );
 }
 
 export default Modal;
