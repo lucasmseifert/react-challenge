c8d41aa9cccaa8b28dd9f362317bfa7f546c77d3
Lucas <lucas.seifert@mmda.com.br>
2021-08-16 11:11:48 -0300

    Add basic structure for the reminder

--- src/components/reminder/Reminder.js
+++ src/components/reminder/Reminder.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Reminder.css';
 
-function Reminder () {
-  return <div>Reminder</div>
+function Reminder ({
+  id,
+  city,
+  color,
+  text,
+  time
+}) {
+  return <div className='reminder' style={{
+    backgroundColor: color,
+  }}>
+    <span className='reminder__time'>{time}</span> - <span className='reminder__text'>{text}</span>
+  </div>
 }
 
 export default Reminder;
