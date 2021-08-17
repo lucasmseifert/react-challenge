46298d6d8ed28f75df3147245b05598b3db0183c
Lucas <lucas.seifert@mmda.com.br>
2021-08-17 05:39:49 -0300

    Add delete reminder button to Reminder component

--- src/components/reminder/Reminder.js
+++ src/components/reminder/Reminder.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Reminder.css';
+import { useCalendarContext } from '../../context/context';
+import { FaTrash } from 'react-icons/fa';
 
-function Reminder ({
+function Reminder({
   id,
   city,
-  color,
+  background,
+  fontColor,
   text,
-  time
+  time,
+  day,
+  monthId,
 }) {
+  const { deleteReminder } = useCalendarContext();
+
   return <div className='reminder' style={{
-    backgroundColor: color,
+    backgroundColor: background,
+    color: fontColor,
   }}>
     <span className='reminder__time'>{time}</span> - <span className='reminder__text'>{text}</span>
+    <button onClick={ () => deleteReminder(monthId, day - 1, id)}><FaTrash/></button>
   </div>
 }
 
