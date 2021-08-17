eb68f6b035322c05a2aef4391c21e8f2bfe8cc51
Lucas <lucas.seifert@mmda.com.br>
2021-08-17 14:17:42 -0300

    Format file

--- src/components/reminder/Reminder.js
+++ src/components/reminder/Reminder.js
@@ -6,7 +6,6 @@ import EditReminderForm from '../editReminderForm/EditReminderForm';
 import { FaPen, FaTrash } from 'react-icons/fa';
 
 function Reminder(props) {
-
   const {
     id,
     city,
@@ -19,19 +18,18 @@ function Reminder(props) {
   } = props;
 
   const { deleteReminder } = useCalendarContext();
-  const [ showEdit, setShowEdit ] = useState(false);
-
+  const [showEdit, setShowEdit] = useState(false);
   return <div className='reminder' style={{
     backgroundColor: background,
     color: fontColor,
   }}>
     <span className='reminder__time'>{time}</span> - <span className='reminder__text'>{text}</span>
-    <button onClick={ () => setShowEdit(true)} aria-label='Edit reminder' title='Edit reminder'><FaPen/></button>
-    <button onClick={ () => deleteReminder(monthId, day - 1, id)} aria-label='Delete reminder' title='Delete reminder'><FaTrash/></button>
+    <button onClick={() => setShowEdit(true)} aria-label='Edit reminder' title='Edit reminder'><FaPen /></button>
+    <button onClick={() => deleteReminder(monthId, day - 1, id)} aria-label='Delete reminder' title='Delete reminder'><FaTrash /></button>
     {
       showEdit && (
         <Modal isOpen={showEdit} setIsOpen={setShowEdit}>
-          <EditReminderForm {...props} closeModal={()=>setShowEdit(false)} />
+          <EditReminderForm {...props} closeModal={() => setShowEdit(false)} />
         </Modal>
       )
     }
