4d7685545f9cf23b75ea030e73c0fa0b6051209e
Lucas <lucas.seifert@mmda.com.br>
2021-08-17 17:25:25 -0300

    Calendar styling

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { useCalendarContext } from "../../context/context";
 import Month from "../month/Month";
-
+import './Calendar.css';
 
 function Calendar() {
   const { shownMonth, changeMonth } = useCalendarContext();
 
-  return <section className='container'>
-    <h1>Calendar: { shownMonth }</h1>
-    <button onClick={() => changeMonth(1)}>+</button>
-    <button onClick={() => changeMonth(-1)}>-</button>
+  return <section className='calendar container'>
+    <div className='calendar__header'>
+      <h1>Calendar: { shownMonth }</h1>
+      <div className='calendar__actions'>
+        <button className='calendar__action button button--icon' onClick={() => changeMonth(-1)}><FaChevronLeft/></button>
+        <button className='calendar__action button button--icon' onClick={() => changeMonth(1)}><FaChevronRight/></button>
+      </div>
+    </div>
     <Month monthId={ shownMonth } />
   </section>
 }
